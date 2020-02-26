555e0ffbcae2b00760fb897662ff7379a5847233
足立守 <rooreckless@gmail.com>
2020-02-26 19:06:32 +0900

    イベントが発火したときにAjaxを使用して、messages#createが動くようにする

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,18 @@ $(function(){
   //$('#new-message').on('submit',function(){
   $('#new_message').on('submit',function(e){
     e.preventDefault();
-    var input = $('#message_content').prop('value');
-    console.log(input);
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    console.log(this);
+    console.log(formData);
+    console.log(url);
+    $.ajax({
+      url: url,  //同期通信でいう『パス』
+      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      data: FormData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    });
   });
 }); 
