38b060d2ed533e5388dc14a5f8befa6c27c266b8
足立守 <rooreckless@gmail.com>
2020-02-26 18:51:03 +0900

    フォームのボタンを押下でイベント発火を確認する

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,2 +1,8 @@
 $(function(){
+  //$('#new-message').on('submit',function(){
+  $('#new_message').on('submit',function(e){
+    e.preventDefault();
+    var input = $('#message_content').prop('value');
+    console.log(input);
+  });
 }); 
