cfe71f73e08fa44b6fc3751a41ee51b29075175a
足立守 <rooreckless@gmail.com>
2020-02-28 04:23:55 +0900

    ajaxレスポンスを受け取ったらHTML化して表示 (仮)
    # what
    ajaxのレスポンスを正しく受け取った.doneメソッドの中で、json=@users=dataをHTML化して表示します。
    # how
    1.#user-search-resultが検索結果を表示するためのdivとしているので、これを消去(.empty())
    2.dataをメソッドapendUserに渡してHTML化してもらいます。
    # important
    このやり方でも問題なく表示されていますが、2.については向学のため後に修正します。
    2のやり方はdata=@usersをまるごと渡していますが、
    後に「data=@usersを1件ずつ、appendHTMLに渡す」やり方に変えます。

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,27 @@
 $(function(){
+  //検索結果一覧なHTML要素を作って表示する。
+  function appendUser(users){
+    var html = "";
+    debugger;
+    //検索してもユーザがひとりもいない場合の要素を作成
+    if (users.length==0){
+      html = `
+            <div class="chat-group-user clearfix">
+              <p class="chat-group-user__name">ユーザーが見つかりません</p>
+            </div>
+                    `;
+    }else{
+      //検索してもユーザがひとりもいない場合の要素を作成
+      users.forEach(function(user){
+      html = html + `
+            <div class="chat-group-user clearfix">
+              <p class="chat-group-user__name">${user.name}</p>
+              <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+            </div>
+            `;});
+    }
+    $("#user-search-result").append(html);
+  }
   $('#user-search-field').on('keyup',function(){
     //「チャットメンバーを追加」の欄を入力した際イベントが発火し以下のコードが実行されます。
     let input = $('#user-search-field').val();
@@ -8,14 +31,12 @@ $(function(){
       dataType: 'json',
       data: { keyword: input } 
     }).done(function(data){
-      console.log("--done--");
-      console.log(data);
-      //ajaxリクエスト通信を$.ajaxで行って、正常に完了したなら、そのレスポンスのjsonをdataとしてうけています。
-      //レスポンスのjsonがどんなものになるのかはview/users/index.json.jbuilderで設定済みです。
-      //コントローラのアクションの現在の設定ではdata=アクションの@usersです。
-      //念のためコンソール出力しました。
+      //まずすでに表示している検索結果部分のHTML要素を消す
+      $('#user-search-result').empty();
+      //検索結果を作成して表示してもらいます。
+      appendUser(data);
     }).fail(function(){
-      alert("エラーが発生しました。");
+      alert("ユーザー検索に失敗しました");
     });
   });
 });
