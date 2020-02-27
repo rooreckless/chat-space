f943db52981844a899d878866c9ee18144ae3bb2
足立守 <rooreckless@gmail.com>
2020-02-28 04:48:22 +0900

    ajaxレスポンスを受け取ったらHTML化して表示 (別解)
    # what
    ajaxのレスポンスを正しく受け取った.doneメソッドの中で、json=@users=dataをHTML化して表示します。
    
    #important
    以前ほぼ同じcommitをしていますが、そこの#howの2を修正するcommitです。
    
    # how
    1. 入力せずに検索した場合や、入力して結果がない場合と結果がある場合にわけます。
    2.1で結果がある場合、forEach文を用い、json１件ずつappendUserメソッドを呼びます。
    このため、doneメソッドの引数を復数形から単数形に書き換えられる、「users」にかえています。
    
    #what learned - javasciptのappendメソッドの挙動について
    
    appendUserは最後に作成した要素を「("#user-search-result").append(作成した要素)」します。
    
     forEachループで呼んだとしても("#user-search-result")の子要素として追加appendするのであり、子要素を上書きするわけではないので、繰り返し呼び出していいです。

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,25 +1,22 @@
 $(function(){
-  //検索結果一覧なHTML要素を作って表示する。
-  function appendUser(users){
-    var html = "";
-    debugger;
+  //検索結果がない時ようのHTML要素を作って表示します。
+  function appendNoUser(){
     //検索してもユーザがひとりもいない場合の要素を作成
-    if (users.length==0){
-      html = `
+    var html = `
             <div class="chat-group-user clearfix">
               <p class="chat-group-user__name">ユーザーが見つかりません</p>
             </div>
-                    `;
-    }else{
-      //検索してもユーザがひとりもいない場合の要素を作成
-      users.forEach(function(user){
-      html = html + `
+            `;
+    $("#user-search-result").append(html);
+  }
+  //検索結果一覧なHTML要素を作って表示する。
+  function appendUser(user){
+    var html = `
             <div class="chat-group-user clearfix">
               <p class="chat-group-user__name">${user.name}</p>
               <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
             </div>
-            `;});
-    }
+            `;
     $("#user-search-result").append(html);
   }
   $('#user-search-field').on('keyup',function(){
@@ -30,11 +27,26 @@ $(function(){
       url: '/users', 
       dataType: 'json',
       data: { keyword: input } 
-    }).done(function(data){
+    }).done(function(users){
       //まずすでに表示している検索結果部分のHTML要素を消す
       $('#user-search-result').empty();
+
       //検索結果を作成して表示してもらいます。
-      appendUser(data);
+      //まず検索結果が0件かどうかで分けます。
+      if (users.length !== 0) {
+        //検索結果がある場合、下のforEach文を使い、１件ずつ、appendUserに渡します。
+        users.forEach(function(user) {
+          appendUser(user);
+          //appendUserは最後に作成した要素を「("#user-search-result").append(作成した要素)」します。
+          //forEachループで呼んだとしても("#user-search-result")の子要素として追加appendするのであり、子要素を上書きするわけではないので、繰り返し呼び出していいです。
+        });
+      } else if (input.length == 0) {
+        //そもそもなんの文字も入力せず(input.length == 0)で検索している場合の処理です。
+        return false;
+      } else {
+        //まず検索しても0件の処理です。
+        appendNoUser();
+      }
     }).fail(function(){
       alert("ユーザー検索に失敗しました");
     });
